116b2af2aa1a099d53d743b0eb67b92dde04e7c4
miracoly <uhlig.marc@icloud.com>
2020-07-25 21:48:39 +0200

    Updates

---
+++ precache-manifest.6d6595d7b6dfe595cbd3fee35c41452e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5dcc810345f8cff6865c1be786dfb7a",
+    "url": "/ztm-robofriends/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/ztm-robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "8be71ac2a1d305c1a006",
+    "url": "/ztm-robofriends/static/css/main.5a29eb5c.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/ztm-robofriends/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ztm-robofriends/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8be71ac2a1d305c1a006",
+    "url": "/ztm-robofriends/static/js/main.1297dc1c.chunk.js"
+  },
+  {
+    "revision": "fc644a321fb87cb9c413",
+    "url": "/ztm-robofriends/static/js/runtime-main.597edd97.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/ztm-robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
